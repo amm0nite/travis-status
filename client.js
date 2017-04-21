c3157258e3ea5ac93da140da7a33b41ddc7b0f63
Ammonite <pierre@ambox.fr>
2017-04-21 21:05:10 +0200

    Add client response handler

--- client.js
+++ client.js
@@ -8,30 +8,30 @@ function defaults() {
     };
 }
 
-function load(key, next) {
+function responseHandler(next) {
     if (!next) {
         next = function() {};
     }
 
-    let params = defaults();
-
-    request('/memory/' + key, params, function(err, res) {
+    return function(err, res) {
         if (err) return next(err);
         if (res.statusCode != 200) return next(res.body);
         return next(null, res.body);
-    });
+    };
+}
+
+function load(key, next) {
+    let params = defaults();
+    
+    request('/memory/' + key, params, responseHandler(next));
 }
 
 function save(key, value, next) {
-    if (!next) {
-        next = function() {};
-    }
-
     let params = defaults();
     params.method = 'POST';
     params.body = value;
 
-    request('/memory/' + key, params, next);
+    request('/memory/' + key, params, responseHandler(next));
 }
 
 function send(payload, options, next) {
@@ -39,7 +39,7 @@ function send(payload, options, next) {
     params.method = 'POST';
     params.body = payload;
 
-    request('/send', params, next);
+    request('/send', params, responseHandler(next));
 }
 
 module.exports = {
